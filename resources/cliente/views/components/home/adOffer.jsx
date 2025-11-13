57ea9319195f16057b511525af80635f206eeaf8
richard <TU_USUARIO@users.noreply.github.com>
2025-11-12 21:41:17 -0400

    Se cambio el atributo class por className para evitar posibles errores.

--- resources/cliente/views/components/home/adOffer.jsx
+++ resources/cliente/views/components/home/adOffer.jsx
@@ -3,10 +3,10 @@ import sty from "../../../css/home/Ads.module.css"
 
 export function AdOffer() {
   return(
-    <article class={`${sty.bg__offer} bg-no-repeat bg-center bg-cover w-full h-69 flex flex-col justify-center gap-3 px-7 rounded-lg`}>
-      <p class="text-white/40 uppercase font-semibold text-xs md:text-sm">Ofertas refrescantes de primavera</p>
-      <h3 class=" text-white font-extrabold text-3xl md:text-4xl w-88">Piezas que trabajan tan duro como tu</h3>
-      <p class=" text-white text-sm font-normal mb-3">Sin desajustes, solo piezas exactas</p>
+    <article className={`${sty.bg__offer} bg-no-repeat bg-center bg-cover w-full h-69 flex flex-col justify-center gap-3 px-7 rounded-lg`}>
+      <p className="text-white/40 uppercase font-semibold text-xs md:text-sm">Ofertas refrescantes de primavera</p>
+      <h3 className=" text-white font-extrabold text-3xl md:text-4xl w-88">Piezas que trabajan tan duro como tu</h3>
+      <p className=" text-white text-sm font-normal mb-3">Sin desajustes, solo piezas exactas</p>
       <ButtonPrimary styles="text-black bg-white hover:from-white hover:to-gray-200" text="Comprar ahora"/>
     </article>
   )
--- resources/cliente/views/components/home/adParts.jsx
+++ resources/cliente/views/components/home/adParts.jsx
@@ -3,10 +3,10 @@ import sty from "../../../css/home/Ads.module.css"
 
 export function AdParts() {
   return(
-    <article class={`${sty.bg__parts} bg-add2 bg-no-repeat bg-center bg-cover w-full h-69 flex flex-col justify-center gap-3 px-7 rounded-lg`}>
-      <p class="text-black/40 uppercase font-semibold text-xs md:text-sm">Precision en cada parte</p>
-      <h3 class="text-black font-extrabold text-3xl md:text-4xl w-95">Repuestos confiables para cada viaje</h3>
-      <p class="text-black text-sm font-normal mb-3">Cada pieza esta diseñada a tu medida</p>
+    <article className={`${sty.bg__parts} bg-add2 bg-no-repeat bg-center bg-cover w-full h-69 flex flex-col justify-center gap-3 px-7 rounded-lg`}>
+      <p className="text-black/40 uppercase font-semibold text-xs md:text-sm">Precision en cada parte</p>
+      <h3 className="text-black font-extrabold text-3xl md:text-4xl w-95">Repuestos confiables para cada viaje</h3>
+      <p className="text-black text-sm font-normal mb-3">Cada pieza esta diseñada a tu medida</p>
       <ButtonPrimary styles="text-black bg-white hover:from-white hover:to-gray-200" text="Comprar ahora"/>
     </article>
   )
