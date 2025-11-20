bfc7ed4a47de8f52a9da07a86c71d681eb9181f5
richard <TU_USUARIO@users.noreply.github.com>
2025-11-20 17:05:12 -0400

    Actualizar rutas para la vista del cliente y agregar rutas de API para la gestión de categorías

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
+use App\Http\Controllers\Api\Admin\CategoryController;
+
+
+/*Route::get('/', function () {
+  return view('admin');
+});*/
 
 Route::get('/', function () {
-  return view('app');
+  return view('cliente');
+});
+
+Route::get('/admin/categorias', function () {
+  return Inertia::render('Admin/Category/Categories'); 
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
@@ -15,3 +25,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
 });
 
 require __DIR__.'/settings.php';
+
+
+// API
+Route::get('/categorias', [CategoryController::class, 'index']);
+Route::post('/categorias/crear', [CategoryController::class, 'store']);
+Route::delete('/categorias/eliminar/{id}', [CategoryController::class, 'destroy']);
+Route::put('/categorias/editar/{id}', [CategoryController::class, 'update']);
