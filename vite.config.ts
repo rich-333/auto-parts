5f52dc113f9fc7a138125825075c16e311b73e5e
richard <TU_USUARIO@users.noreply.github.com>
2025-11-20 08:12:34 -0400

    Agregar archivos de configuración para el despliegue y la ejecución del servidor, incluyendo Procfile y railway.json. Actualizar la configuración de Vite para incluir nuevos archivos de entrada.

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
+            input: [
+                'resources/css/app.css', 
+                'resources/cliente/AppCliente.jsx',
+                'resources/admin/AppAdmin.jsx'
+            ],
             ssr: 'resources/js/ssr.tsx',
             refresh: true,
         }),
