5a001753beba7ccf55de861227a2dcd48099286e
richard <TU_USUARIO@users.noreply.github.com>
2025-11-20 18:43:40 -0400

    Se cambiaron las rutas de las imagenes ya que estas no cargaban en el despliegue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
     {
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
+            
+            $this->app['request']->server->set('HTTPS', 'on');
         }
     }
 }
